a88788d3cf76bc5fdedd920a2265eefcf5778b94
reza-sadrinia <reza.sadriniaa@gmail.com>
2022-09-09 21:21:34 +0430

    Update UserProfile(102)

--- app/http/middleware/autologin.js
+++ app/http/middleware/autologin.js
@@ -4,13 +4,13 @@ const { verifyjwtToken } = require("../../modules/function")
 const checklogin = async (req, res, next) => {
   try {
     const authorization = req?.headers?.authorization
-    console.log(authorization);
+    // console.log(authorization);
     if (!authorization) throw { status: 401, message: "authorization not Found" }
     let token = authorization.split(" ")?.[1]
     if (!token) throw { status: 401, message: "token not found" }
     const result = verifyjwtToken(token)
     const { username } = result
-    console.log(result);
+    // console.log(result);
     const user = await UserModel.findOne({ username }, { password: 0 })
     if (!user) throw { status: 401, message: "user not found" }
     req.user = user
--- app/router/user.js
+++ app/router/user.js
@@ -3,6 +3,7 @@ const { checklogin } = require("../http/middleware/autologin")
 const router = require("express").Router()
 
 router.get("/profile", checklogin, UserController.getProfile)
+router.post("/profile", checklogin, UserController.editProfile)
 
 module.exports = {
   userRoutes: router
