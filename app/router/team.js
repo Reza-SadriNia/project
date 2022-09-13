6c2290633289cdaac3ffa0bc72bd63367c77351a
reza-sadrinia <reza.sadriniaa@gmail.com>
2022-09-13 22:09:15 +0430

    Create Team and Get all Teams

--- app/router/team.js
+++ app/router/team.js
@@ -1,4 +1,11 @@
+const { TeamController } = require("../http/controllers/team.controller")
+const { checklogin } = require("../http/middleware/autologin")
+const { createteamValidation } = require("../http/validation/team")
+const { expressValidatorMapper } = require("../http/middleware/checkErrors")
 const router = require("express").Router()
+
+router.post("/craete", checklogin, createteamValidation(), expressValidatorMapper, TeamController.createTeam)
+router.get("/getListOfTeams", checklogin, expressValidatorMapper, TeamController.getListOfTeams)
 
 module.exports = {
   teamRoutes: router
